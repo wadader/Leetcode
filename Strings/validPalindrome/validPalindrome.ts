374c5461b839af779ffc985f39cd5b9e1cc2f92e
Muhammad Bin Aamir <Mbaamir21@gmail.com>
2023-08-06 19:39:16 +0500

    feat: improve validPalindrome

--- Strings/validPalindrome/validPalindrome.ts
+++ Strings/validPalindrome/validPalindrome.ts
@@ -1,27 +1,14 @@
 function isPalindrome(s: string): boolean {
-  let processedString = s.trim();
-  const regexPattern = /[^A-Za-z0-9]/g;
-  processedString = processedString.replace(regexPattern, "");
-  processedString = processedString.toLowerCase();
+  const sanitizedString = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
 
-  const stringLength = processedString.length;
-
-  for (let i = 0; i < stringLength; i++) {
-    const rightPointer = stringLength - 1 - i;
-    const leftPointer = i;
-
-    if (rightPointer === leftPointer) break;
-
-    if (processedString[rightPointer] !== processedString[leftPointer])
-      return false;
+  let left = 0;
+  let right = sanitizedString.length - 1;
+  while (left < right) {
+    if (sanitizedString[left] !== sanitizedString[right]) return false;
+    right--;
+    left++;
   }
   return true;
-}
-
-function isAlphanumeric(code: number) {
-  // digits: 48-57
-  // lowercase letters: 97-122
-  return (code >= 48 && code <= 57) || (code >= 97 && code <= 122);
 }
 
 export default isPalindrome;
